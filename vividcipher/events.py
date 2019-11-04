650c9495b09571e2b7285397f966e9ca3a073845
Amrit Pandey <amrit.github@tuta.io>
2019-11-04 13:47:09 +0530

    developer role will be added to new members

--- vividcipher/events.py
+++ vividcipher/events.py
@@ -1,25 +1,38 @@
+#!/usr/bin/python3
+
+from discord import HTTPException
 from vividcipher.client import bot
-from vividcipher.config import channels
+from vividcipher.config import channels, roles
 
-msg = "Welcome to {}, <@{}>! Have a look at server info & coc in <#{}> and <#{}>. If you need help with anything, start posting your questions in <#{}>."
+msg = "Welcome to __{}__, <@{}>! Have a look at server info & coc in <#{}> and <#{}>. "
+msg += "If you need help with anything, start posting your questions in <#{}>."
 
-
+# ON_MEMBER_JOIN
 @bot.event
 async def on_member_join(member):
     guild = member.guild
-    for c in guild.channels:
-        if c.name == "welcome-🎊":
-            await c.send(
-                msg.format(
-                    guild.name,
-                    member.id,
-                    channels["information"],
-                    channels["netiquette"],
-                    channels["general"],
-                )
-            )
+    welcome_chl = guild.get_channel(int(channels["welcome"]))  # id casted into int type
+    bot_chl = guild.get_channel(int(channels["bot"]))
+    dev_role = guild.get_role(int(roles["developer"]))
+
+    try:
+        await member.add_roles(dev_role, reason="new member")
+    except HTTPException:
+        print("error: http, adding role failed")
+        await bot_chl.send("failure: failed to set role for <@{}>".format(member.id))
+
+    await welcome_chl.send(
+        msg.format(
+            guild.name,
+            member.id,
+            channels["information"],
+            channels["netiquette"],
+            channels["general"],
+        )
+    )
 
 
+# ON_BOT_READY
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
